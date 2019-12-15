5e875f311db655bb18b9c5cf2aff3888d36bffe2
gyucode <gyucode@gmail.com>
2019-12-15 11:43:14 +0900

    스크롤뷰 사용 체험

--- app/src/main/java/com/example/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191215_02_signuppractice/SignUpActivity.kt
@@ -1,21 +1,27 @@
 package com.example.a20191215_02_signuppractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+//SignUpActivity로 변경
+class SignUpActivity : BaseActivity() {
+
+    var lastBackTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 
 
     override fun setEvents() {
 
+        lastBackTime = 10L
+
     }
 
     override fun setValue() {
+
+        lastBackTime = 20L
 
     }
 
