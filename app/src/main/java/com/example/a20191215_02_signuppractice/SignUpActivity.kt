51579128b82f91c5d049bc82f31d35f95ac93e7a
gyucode <gyucode@gmail.com>
2019-12-15 12:32:26 +0900

    Color클래스를 이용해서 글씨 색 변경

--- app/src/main/java/com/example/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191215_02_signuppractice/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a20191215_02_signuppractice
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -26,11 +27,14 @@ class SignUpActivity : BaseActivity() {
             val inputStr = it.toString()
             if(inputStr.length == 0 ){
                 pwStateTxt.text = "비밀번호가 입력되지 않았습니다."
+                pwStateTxt.setTextColor(Color.RED)
             }
             else if (inputStr.length < 8){
                 pwStateTxt.text = "입력된 비번이 너무 짧습니다"
+                pwStateTxt.setTextColor(Color.parseColor("#FDA0EF"))
             }
             else{
+                pwStateTxt.setTextColor(Color.GREEN)
                 pwStateTxt.text = "사용해도 좋은 비밀번호입니다."
             }
         }
